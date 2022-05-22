b5d42b4e3cd86403ead19fa3f8e3ca5e53d9cf8f
Pablo Carvalho <pablocarvalho.dev@gmail.com>
2022-05-22 14:38:28 -0300

    Preparando para começar o site

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def about(request):
-    return HttpResponse('Página sobre')
-
-
-def contact(request):
-    return HttpResponse('Página de contato')
+    context = {
+        'name': 'Pablo Carvalho'
+    }
+    return render(request, 'recipes/pages/home.html', context=context)
